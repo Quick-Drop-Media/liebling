4beba14c5f6623739beb46fa285149b4a15c57c3
Matthew Schmoyer <mattschmo@gmail.com>
2021-01-18 00:33:34 -0700

    Adds issue and story collections * Adds home page * index.hbs is used for stories collection * Adds blank contact page * Issue may be renamed to "newsletter" or "daily drop" in the future

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -28,6 +28,7 @@ mix.js('js/helpers.js', 'js/')
   .sass('sass/home.scss', 'css/')
   .sass('sass/listing.scss', 'css/')
   .sass('sass/post.scss', 'css/')
+  .sass('sass/issue.scss', 'css/')
   .sass('sass/tags.scss', 'css/')
   .sass('sass/404.scss', 'css/')
   .options({
